cd542e1f6c786f58bc3b10a5c0fffa02bd502c73
Rob Orndoff <rorndoff@gmail.com>
2019-09-30 15:02:34 -0400

    Basic Requirements

---
+++ App.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PasswordEncryption
+{
+    public class App
+    {
+        public void Run()
+        {
+            var usernameDict = new Dictionary<string, string>();
+            MainMenu(usernameDict);
+        }
+
+        private static void MainMenu(Dictionary<string, string> usernameDict)
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------------------------\n");
+            Console.WriteLine("PASSWORD AUTHENTICATION SYSTEM\n");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Establish an account");
+            Console.WriteLine("2. Authenticate a user");
+            Console.WriteLine("3. Exit the system");
+            Console.Write("\nEnter a selection: ");
+            var menuOption = Console.ReadLine();
+            Console.WriteLine("\n\n--------------------------------------------------------------------\n");
+            if (menuOption == "1")
+            {
+                MakeAccount(usernameDict);
+            }
+            else if (menuOption == "2")
+            {
+                Authenticate(usernameDict);
+            }
+            else if (menuOption == "3")
+            {
+                Console.WriteLine("All usernames and passwords have been deleted. Goodbye.");
+                Exit();
+            }
+            else
+            {
+                Console.WriteLine("Not a valid choice.  Press any key to continue...");
+                Console.ReadKey();
+                MainMenu(usernameDict);
+            }
+        }
+
+        public static void MakeAccount(Dictionary<string,string> usernameDict)
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------------------------\n");
+            Console.WriteLine("CREATE A NEW USER\n");
+            Console.Write("Enter a username: ");
+            var username = Console.ReadLine();
+            Console.Write("Enter a password: ");
+            var password = Console.ReadLine();
+            password = GetHash(password);
+            usernameDict.Add(username, password);
+            Console.WriteLine("\n\n--------------------------------------------------------------------\n");
+
+            Console.WriteLine($"The following user was added:");
+            Console.WriteLine($"Username: {username}");
+            Console.WriteLine($"Password (Hashed): {usernameDict[username]}");
+            Console.WriteLine("\n");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            MainMenu(usernameDict);
+        }
+        public static void Authenticate(Dictionary<string, string> usernameDict)
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------------------------------------\n");
+            Console.WriteLine("AUTHENTICATE A USER\n");
+            Console.Write("Enter a username: ");
+            var username = Console.ReadLine();
+
+
+            var passCorrect = false;
+            var count = 3;
+            do
+            {
+                Console.Write("Enter a password: ");
+                var password = Console.ReadLine();
+                password = GetHash(password);
+                var hashedPassword = usernameDict[username];
+                if (hashedPassword == password)
+                {
+                    Console.WriteLine("You have successfully authenticated; the password matched.");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                    passCorrect = true;
+                }
+                else
+                {
+                    count--;
+                    Console.WriteLine("\nIncorrect password.");
+                    Console.WriteLine($"You have {count} attempts remaining...");
+                }
+            } while (passCorrect == false && count != 0);
+
+            Console.WriteLine("\n\n--------------------------------------------------------------------\n");
+            MainMenu(usernameDict);
+        }
+        public static void Exit()
+        {;}
+        internal static string GetHash(string password)
+        {
+            if (String.IsNullOrEmpty(password))
+                return String.Empty;
+
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                byte[] textData = System.Text.Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha.ComputeHash(textData);
+                return BitConverter.ToString(hash).Replace("-", String.Empty);
+            }
+        }
+    }
+}
