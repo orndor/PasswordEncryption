1a16190bcf955164a1bad4b70213f1f4d1583b0f
Rob Orndoff <rorndoff@gmail.com>
2019-09-30 15:24:48 -0400

    All requirements integrated.

--- App.cs
+++ App.cs
@@ -33,7 +33,6 @@ namespace PasswordEncryption
             }
             else if (menuOption == "3")
             {
-                Console.WriteLine("All usernames and passwords have been deleted. Goodbye.");
                 Exit();
             }
             else
@@ -44,64 +43,93 @@ namespace PasswordEncryption
             }
         }
 
-        public static void MakeAccount(Dictionary<string,string> usernameDict)
+        private static void MakeAccount(Dictionary<string,string> usernameDict)
         {
-            Console.Clear();
-            Console.WriteLine("--------------------------------------------------------------------\n");
-            Console.WriteLine("CREATE A NEW USER\n");
-            Console.Write("Enter a username: ");
-            var username = Console.ReadLine();
-            Console.Write("Enter a password: ");
-            var password = Console.ReadLine();
-            password = GetHash(password);
-            usernameDict.Add(username, password);
-            Console.WriteLine("\n\n--------------------------------------------------------------------\n");
 
-            Console.WriteLine($"The following user was added:");
-            Console.WriteLine($"Username: {username}");
-            Console.WriteLine($"Password (Hashed): {usernameDict[username]}");
-            Console.WriteLine("\n");
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            MainMenu(usernameDict);
-        }
-        public static void Authenticate(Dictionary<string, string> usernameDict)
-        {
-            Console.Clear();
-            Console.WriteLine("--------------------------------------------------------------------\n");
-            Console.WriteLine("AUTHENTICATE A USER\n");
-            Console.Write("Enter a username: ");
-            var username = Console.ReadLine();
-
-
-            var passCorrect = false;
-            var count = 3;
-            do
+            try
             {
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------------------------\n");
+                Console.WriteLine("CREATE A NEW USER\n");
+                Console.Write("Enter a username: ");
+                var username = Console.ReadLine();
                 Console.Write("Enter a password: ");
                 var password = Console.ReadLine();
                 password = GetHash(password);
-                var hashedPassword = usernameDict[username];
-                if (hashedPassword == password)
-                {
-                    Console.WriteLine("You have successfully authenticated; the password matched.");
-                    Console.WriteLine("Press any key to continue...");
-                    Console.ReadKey();
-                    passCorrect = true;
-                }
-                else
-                {
-                    count--;
-                    Console.WriteLine("\nIncorrect password.");
-                    Console.WriteLine($"You have {count} attempts remaining...");
-                }
-            } while (passCorrect == false && count != 0);
+                usernameDict.Add(username, password);
+                Console.WriteLine("\n\n--------------------------------------------------------------------\n");
 
-            Console.WriteLine("\n\n--------------------------------------------------------------------\n");
-            MainMenu(usernameDict);
+                Console.WriteLine($"The following user was added:");
+                Console.WriteLine($"Username: {username}");
+                Console.WriteLine($"Password (SHA256 Hash): {usernameDict[username]}");
+                Console.WriteLine("\n");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                MainMenu(usernameDict);
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("\nThat username is already in the data store.  Please try again.");
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+            }
+            finally
+            {
+                MainMenu(usernameDict);
+            }
+
         }
-        public static void Exit()
-        {;}
+        private static void Authenticate(Dictionary<string, string> usernameDict)
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("--------------------------------------------------------------------\n");
+                Console.WriteLine("AUTHENTICATE A USER\n");
+                Console.Write("Enter a username: ");
+                var username = Console.ReadLine();
+
+                var passCorrect = false;
+                var count = 3;
+                do
+                {
+                    Console.Write("Enter a password: ");
+                    var password = Console.ReadLine();
+                    password = GetHash(password);
+                    var hashedPassword = usernameDict[username];
+                    if (hashedPassword == password)
+                    {
+                        Console.WriteLine("You have successfully authenticated; the password matched.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        passCorrect = true;
+                    }
+                    else
+                    {
+                        count--;
+                        Console.WriteLine("\nIncorrect password.");
+                        Console.WriteLine($"You have {count} attempts remaining...");
+                    }
+                } while (passCorrect == false && count != 0);
+
+                Console.WriteLine("\n\n--------------------------------------------------------------------\n");
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.WriteLine("\nThat username is not in the data store.  Please try again.");
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+            }
+            finally
+            {
+                MainMenu(usernameDict);
+            }
+        }
+        private static void Exit()
+        {
+            Console.WriteLine("All usernames and passwords have been deleted. Goodbye.");
+            Environment.Exit(0);
+        }
         internal static string GetHash(string password)
         {
             if (String.IsNullOrEmpty(password))
